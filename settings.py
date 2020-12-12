7fa8ba20c643be12357a2f08a49b836052dfe10d
volodymyrb <volodymyrb@fornova.net>
2020-12-12 23:22:59 +0200

    refactor

--- settings.py
+++ settings.py
@@ -4,9 +4,5 @@ SQL_LITE_SETTINGS = {
     'create_db': True,
 }
 
-PONY_SETTINGS = {
-    'sqlite': SQL_LITE_SETTINGS,
-}
-
 TOKEN = '1312220937:AAFfFoK2qDsoeyDEvSN3zK3RWOA20cQMdzY'
 DEFAULT_TOPIC = 'Tennis'
