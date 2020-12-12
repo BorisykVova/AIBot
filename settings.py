c789cc96102c593604291c4c575406926a9de969
volodymyrb <volodymyrb@fornova.net>
2020-12-12 23:38:36 +0200

    external telegram TOKEN

--- settings.py
+++ settings.py
@@ -1,8 +1,11 @@
+import os
+
 SQL_LITE_SETTINGS = {
     'provider': 'sqlite',
     'filename': 'sqlite.db',
     'create_db': True,
 }
 
-TOKEN = '1312220937:AAFfFoK2qDsoeyDEvSN3zK3RWOA20cQMdzY'
 DEFAULT_TOPIC = 'Tennis'
+
+TOKEN = os.getenv('TOKEN')
